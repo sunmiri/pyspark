2cf36d9f7b3c56d6d9cbe428c899454b9a216299
sunil.miriyala@cloudfabrix.com <sunil.miriyala@cloudfabrix.com>
2020-08-11 18:26:27 -0500

    for mac

--- emr_bootstrap.sh
+++ emr_bootstrap.sh
@@ -1,17 +1,10 @@
 #!/bin/bash -xe
 
-#sudo yum install -y python37 python37-pip
-
-python --version
-#python -m pip install pyspark jproperties
-
-python3 --version
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3 get-pip.py
-pip3 install pyspark jproperties
-
 export SPARK_HOME=/usr/lib/spark
 export PYSPARK_PYTHON=/usr/bin/python3
 
-#https://aws.amazon.com/premiumsupport/knowledge-center/emr-pyspark-python-3x/
-#sudo sed -i -e '$a\export PYSPARK_PYTHON=/usr/bin/python3' /etc/spark/conf/spark-env.sh
+python3 --version
+
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py
+pip3 install pyspark jproperties
--- setenv.sh
+++ setenv.sh
@@ -1,18 +1,16 @@
-export APP_HOME_DIR=/mnt/c/Training/PySpark/pyspark
-#$(dirname "$0")/../
-cd $APP_HOME_DIR
-APP_HOME_DIR=$(pwd)
+export APP_HOME_DIR=/Users/sunilmiriyala/CirrusSS/A-Cloud/Training/pyspark/
+echo "APP_HOME_DIR $APP_HOME_DIR"
 
 export SPARK_HOME=$APP_HOME_DIR/lib/spark-3.0.0-bin-hadoop3.2
 export PATH=$PATH:$APP_HOME_DIR/lib/spark-3.0.0-bin-hadoop3.2/bin
-export PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/lib/py4j-0.10.4-src.zip:$PYTHONPATH
+export PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/lib/py4j-0.10.9-src.zip:$PYTHONPATH
 export PATH=$SPARK_HOME/python:$PATH
 echo "SPARK_HOME $SPARK_HOME"
 echo "PATH $PATH"
 
-mkdir -p $APP_HOME_DIR/venv/
-python3 -m venv $APP_HOME_DIR/venv
-source $APP_HOME_DIR/venv/bin/activate
+cd $APP_HOME_DIR
+python3 -m venv venv
+source ./venv/bin/activate
 python --version
 pip --version
 pip install pyspark jproperties
