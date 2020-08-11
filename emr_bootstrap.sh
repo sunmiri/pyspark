a990e92aae5695846dd48d493f0828bfa966178f
sunmiri <69444662+sunmiri@users.noreply.github.com>
2020-08-11 01:46:40 -0500

    Add files via upload

--- emr_bootstrap.sh
+++ emr_bootstrap.sh
@@ -8,7 +8,7 @@ python --version
 python3 --version
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
-pip3 install boto3 pyspark jproperties
+pip3 install pyspark jproperties
 
 export SPARK_HOME=/usr/lib/spark
 export PYSPARK_PYTHON=/usr/bin/python3
