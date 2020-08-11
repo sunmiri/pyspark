60c0440c09cd4d33c47200102a99cdeee98376d6
sunmiri <69444662+sunmiri@users.noreply.github.com>
2020-08-11 18:40:46 -0500

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -1,6 +1,5 @@
 export APP_HOME_DIR=/Users/sunilmiriyala/CirrusSS/A-Cloud/Training/pyspark/
 echo "APP_HOME_DIR $APP_HOME_DIR"
-
 export SPARK_HOME=$APP_HOME_DIR/lib/spark-3.0.0-bin-hadoop3.2
 export PATH=$PATH:$APP_HOME_DIR/lib/spark-3.0.0-bin-hadoop3.2/bin
 export PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/lib/py4j-0.10.9-src.zip:$PYTHONPATH
