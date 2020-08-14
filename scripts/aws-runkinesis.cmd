bc0ce622fcef707076255b5b9256bc25b05542ca
sunil.miriyala@cloudfabrix.com <sunil.miriyala@cloudfabrix.com>
2020-08-14 16:12:47 -0500

    sync

--- scripts/aws-runkinesis.cmd
+++ scripts/aws-runkinesis.cmd
@@ -12,7 +12,7 @@ aws emr create-cluster --name "SparkStep-RunKinesis" ^
     --instance-type m4.large ^
     --instance-count 2 ^
     --bootstrap-actions Path=s3://pyspark-sunil/aws_bootstrap.sh ^
-    --steps Type=Spark,Name="Spark-Job-RunKinesis",ActionOnFailure=CONTINUE,Args=[--deploy-mode,client,--master,yarn,--jars,s3://pyspark-sunil/lib/spark-streaming-kinesis-asl-assembly_2.12-3.0.0.jar,s3://pyspark-sunil/run/pyspark-kinesis.py,--p,/tmp/pyspark-kinesis.properties] ^
+    --steps Type=Spark,Name="Spark-Job-RunKinesis",ActionOnFailure=CONTINUE,Args=[--deploy-mode,client,--master,yarn,--num-executors,2,--executor-cores,2,--jars,s3://pyspark-sunil/lib/spark-streaming-kinesis-asl-assembly_2.12-3.0.0.jar,s3://pyspark-sunil/run/pyspark-kinesis.py,--p,/tmp/pyspark-kinesis.properties] ^
     --use-default-roles ^
     --auto-terminate
 
