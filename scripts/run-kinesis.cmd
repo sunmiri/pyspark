bdc04d17b5d2bab3933f0b608eef738c3dd5b369
sunil.miriyala@cloudfabrix.com <sunil.miriyala@cloudfabrix.com>
2020-08-20 16:53:44 -0500

    cmd file

---
+++ scripts/run-kinesis.cmd
@@ -0,0 +1,13 @@
+set APP_HOME_DIR=%APP_HOME_DIR%
+cd %APP_HOME_DIR%
+echo "JAVA_HOME %JAVA_HOME%"
+echo "SPARK_HOME %SPARK_HOME%"
+echo "PATH %PATH%"
+
+echo "Starting Program"
+cd %APP_HOME_DIR%\src\python\
+rem Option;1
+%SPARK_HOME%\bin\spark-submit --master local[4] --jars "%APP_HOME_DIR%\lib\spark-streaming-kinesis-asl-assembly_2.12-3.0.0.jar,%APP_HOME_DIR%\lib\spark-avro_2.12-3.0.0.jar,%APP_HOME_DIR%\lib\RedshiftJDBC42-no-awssdk-1.2.45.1069.jar" %APP_HOME_DIR%\src\python\pyspark-kinesis.py --p %APP_HOME_DIR%\scripts\pyspark-kinesis.properties
+
+rem Option;2
+rem pyspark pyspark-csv.py %APP_HOME_DIR%\src\python\pyspark-csv.properties
--- scripts/run-readcsv.cmd
+++ scripts/run-readcsv.cmd
@@ -7,7 +7,7 @@ echo "PATH %PATH%"
 echo "Starting Program"
 cd %APP_HOME_DIR%\src\python\
 rem Option;1
-%SPARK_HOME%\bin\spark-submit --jars "%APP_HOME_DIR%\lib\spark-streaming-kinesis-asl-assembly_2.12-3.0.0.jar,%APP_HOME_DIR%\lib\spark-avro_2.12-3.0.0.jar" %APP_HOME_DIR%\src\python\pyspark-csv.py --p %APP_HOME_DIR%\scripts\pyspark-csv.properties
+%SPARK_HOME%\bin\spark-submit --master local[4] --jars "%APP_HOME_DIR%\lib\spark-streaming-kinesis-asl-assembly_2.12-3.0.0.jar,%APP_HOME_DIR%\lib\spark-avro_2.12-3.0.0.jar" %APP_HOME_DIR%\src\python\pyspark-csv.py --p %APP_HOME_DIR%\scripts\pyspark-csv.properties
 
 rem Option;2
 rem pyspark pyspark-csv.py %APP_HOME_DIR%\src\python\pyspark-csv.properties
