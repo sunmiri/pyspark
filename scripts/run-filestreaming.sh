b6b2f5d03f9152b7d59a59d51b337d631532fccd
sunil.miriyala@cloudfabrix.com <sunil.miriyala@cloudfabrix.com>
2020-08-27 18:45:35 -0500

    file-streaming

---
+++ scripts/run-filestreaming.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+export APP_HOME_DIR=/Users/sunilmiriyala/CirrusSS/A-Cloud/Training/pyspark
+
+cd $APP_HOME_DIR
+if [[ -d "$APP_HOME_DIR/venv" ]]
+then
+    echo "Sourcing Python Virtual Env"
+    source $APP_HOME_DIR/venv/bin/activate
+    pip install pyspark jproperties argparse json
+else
+    echo "Creating Virtual Env in current dir $(pwd)"
+    python3 -m venv venv
+    source $APP_HOME_DIR/venv/bin/activate
+    pip install pyspark jproperties argparse json
+fi
+
+#Runtime: it picks from <home>/.aws/credentials (aws configure)
+#export AWS_ACCESS_KEY_ID=AKIA6AACCFDFGQHBQMNG
+#export AWS_SECRET_ACCESS_KEY=
+export SPARK_HOME=$APP_HOME_DIR/lib/spark-3.0.0-bin-hadoop3.2
+export PATH=$PATH:$APP_HOME_DIR/lib/spark-3.0.0-bin-hadoop3.2/bin
+export PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/lib/py4j-0.10.9-src.zip:$PYTHONPATH
+export PATH=$SPARK_HOME/python:$PATH
+echo "SPARK_HOME $SPARK_HOME"
+echo "PYTHONPATH: $PYTHONPATH"
+echo "PATH $PATH"
+
+cd $APP_HOME_DIR/src/python/
+#Option:1
+$SPARK_HOME/bin/spark-submit --master local[4] \
+    --jars "$APP_HOME_DIR/lib/spark-streaming-kinesis-asl-assembly_2.12-3.0.0.jar" \
+    pyspark-stream-files.py --p $APP_HOME_DIR/scripts/pyspark-stream-files.properties
---
+++ src/python/pyspark-stream-files.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  9 11:21:19 2020
+
+@author: sunilmiriyala
+"""
+#https://spark.apache.org/docs/latest/api/python/index.html
+
+import sys
+import pyspark
+from pyspark.streaming.kinesis import KinesisUtils, InitialPositionInStream
+from pyspark import SparkContext, SparkConf
+from pyspark.streaming import StreamingContext
+from jproperties import Properties
+from pyspark.storagelevel import StorageLevel 
+from pyspark.sql.types import Row
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, ArrayType
+from pyspark.sql.functions import pandas_udf, PandasUDFType
+import pandas as pd
+
+#https://spark.apache.org/docs/latest/api/python/index.html
+#http://spark.apache.org/docs/latest/streaming-kinesis-integration.html
+
+#https://docs.aws.amazon.com/cli/latest/reference/kinesis/put-record.html
+#aws kinesis put-record --stream-name pyspark-kinesis --cli-binary-format raw-in-base64-out --data "{'name':'sunil'}" --partition-key test
+
+#https://aws.amazon.com/blogs/big-data/optimize-spark-streaming-to-efficiently-process-amazon-kinesis-streams/
+class MyPySparkApp:
+    def __init__(self, **kwargs):
+        print("__init__::kwargs:%s" % kwargs)
+        self.appname=kwargs.get("spark.name", "MyPySparkKinesis")
+        self.master=kwargs.get("spark.master", "local")
+        self.src_type=kwargs.get("data.source.type", "kinesis")
+        self.src_format=kwargs.get("data.source.format", "json")
+        self.batch_dur_sec=kwargs.get("spark.stream.batch.duration.secs", 5)
+
+        self.src_folder = kwargs.get("source.folder")
+        self.sink_folder = kwargs.get("sink.folder")
+        
+        self.conf = SparkConf().setAppName(self.appname)
+        print("__init__::self.conf:%s" % self.conf)
+
+        self.sc = SparkContext(conf=self.conf)
+        print("__init__::self.sc:%s" % self.sc)
+
+        self.ssc = StreamingContext(self.sc, int(self.batch_dur_sec))
+        print("__init__::self.ssc:%s" % (self.ssc))
+
+        self.spark = SparkSession.builder.config(conf=self.conf).getOrCreate()
+        self.spark.sparkContext.setLogLevel("ERROR")
+        print("__init__::self.spark:%s" % self.spark)
+
+        self.data_schema = StructType([StructField('emp_number', IntegerType(), True), StructField('emp_name', StringType(), True), StructField('dept_name', StringType(), True), StructField('emp_id', IntegerType(), True), StructField('dept_id', IntegerType(), True)])
+    
+    #https://spark.apache.org/docs/latest/api/python/_modules/pyspark/sql/streaming.html#DataStreamWriter.format
+    def startApp(self):
+        print("startApp")
+
+        def udfDistinctAggregation(key, pdf):
+            #print("udfDistinctAggregation::pdf:", type(pdf), key, pdf)
+            #print("udfDistinctAggregation::key:", key)
+            #print("udfDistinctAggregation::pdf:", pdf)
+            #pd.DataFrame([key + (pdf.v.mean(),)])
+            _pdf = pdf.groupby(['emp_name'])['dept_name'].apply(lambda x: ','.join(x)).reset_index()
+            print("udfDistinctAggregation::_pdf:", _pdf)
+            return _pdf[['emp_name']]
+
+        #spark.udf.register("distAggUDF", udfDistinctAggregation,StringType())
+        #distAggUDF = udf(lambda c: udfDistinctAggregation(c),StringType())
+        csvDF = self.spark.readStream.csv(path=self.src_folder, schema=self.data_schema)
+        #withColumn("agg_dept_name", (distAggUDF(col("dept_name"))))
+        csvDF = csvDF.groupBy(col("emp_name")).applyInPandas(udfDistinctAggregation, schema="emp_name string")
+        csvDF.writeStream.format("console").outputMode("append").start().awaitTermination()
+        
+    def start(self):
+        self.startApp()
+        
+if __name__ == '__main__':
+    import json
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--p", help="Properties File", dest='prop', required=True)
+    args = parser.parse_args()
+    kw = {}
+    configs = Properties()
+    with open(args.prop, 'rb') as config_file:
+        configs.load(config_file)
+    for p in configs:
+        print("P:Name:%s, Val:%s" % (p, configs.get(p).data))
+        kw[p] = configs.get(p).data
+    print("configs::%s" % configs)
+    app = MyPySparkApp(**kw)
+    app.start()
