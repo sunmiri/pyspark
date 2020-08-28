904947bf020b8863675c880328474670065a9ae3
sunil.miriyala@cloudfabrix.com <sunil.miriyala@cloudfabrix.com>
2020-08-28 16:54:44 -0500

    file streaming cmd

---
+++ scripts/run-filestreaming.cmd
@@ -0,0 +1,10 @@
+set APP_HOME_DIR=%APP_HOME_DIR%
+cd %APP_HOME_DIR%
+echo "JAVA_HOME %JAVA_HOME%"
+echo "SPARK_HOME %SPARK_HOME%"
+echo "PATH %PATH%"
+
+echo "Starting Program"
+cd %APP_HOME_DIR%\src\python\
+rem Option;1
+%SPARK_HOME%\bin\spark-submit --master local[4] --jars "%APP_HOME_DIR%\lib\spark-streaming-kinesis-asl-assembly_2.12-3.0.0.jar" %APP_HOME_DIR%\src\python\pyspark-stream-files.py --p %APP_HOME_DIR%\scripts\pyspark-stream-files.properties
